18564016a42b3fd5ba1f7ab70610dec0496a7780
Federico Vazzoler <federico.vazzoler@ts.infn.it>
2020-03-16 17:38:16 +0100

    add code for Nuisance parameter impacts

---
+++ NuisanceImpacts/run_NuisanceImpacts.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+BOSON=$1
+CHANNEL=$2
+YEAR=$3
+
+FOLDER="../html/combine_plots/likelihood_scan/NuisanceImpacts/${BOSON}_${CHANNEL}_${YEAR}"
+mkdir -p ${FOLDER}
+
+#clean
+rm ${BOSON}_${CHANNEL}_${YEAR}_workspace.root
+rm combine_logger.out
+rm ${BOSON}_${CHANNEL}_${YEAR}_impacts.json
+rm higgsCombine_initialFit_Test*
+rm higgsCombine_paramFit_Test*
+rm ${BOSON}_${CHANNEL}_${YEAR}_impacts.pdf
+
+#build workspace
+text2workspace.py ../cards/${BOSON}_${CHANNEL}_${YEAR}_datacard.txt  -o ${BOSON}_${CHANNEL}_${YEAR}_workspace.root
+cp ../cards/${BOSON}_${CHANNEL}_${YEAR}_datacard.txt ${FOLDER} 
+
+#initial fit of mu
+combineTool.py -M Impacts -d ${BOSON}_${CHANNEL}_${YEAR}_workspace.root -m 200 --rMin -10 --rMax 10 --robustFit 1 --doInitialFit
+
+#fit separately every nuisance
+combineTool.py -M Impacts -d ${BOSON}_${CHANNEL}_${YEAR}_workspace.root -m 200 --rMin -10 --rMax 10 --robustFit 1 --doFits
+
+#collect output and convert to json file
+combineTool.py -M Impacts -d ${BOSON}_${CHANNEL}_${YEAR}_workspace.root -m 200 --rMin -10 --rMax 10 --robustFit 1 --output ${BOSON}_${CHANNEL}_${YEAR}_impacts.json
+
+#plot the result
+plotImpacts.py -i ${BOSON}_${CHANNEL}_${YEAR}_impacts.json -o ${BOSON}_${CHANNEL}_${YEAR}_impacts -t rename.json
+cp ${BOSON}_${CHANNEL}_${YEAR}_impacts.pdf ${FOLDER}
---
+++ NuisanceImpacts/run_all.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+BOSONS="WGG ZGG"
+
+CHANNELS="ch_ele ch_muo"
+
+YEARS="2016 2017 2018 Run2"
+
+for BOSON in ${BOSONS}; do
+  for CHANNEL in ${CHANNELS}; do
+    for YEAR in ${YEARS}; do
+      echo "--- ${BOSON} - ${CHANNEL} - ${YEAR} ---"
+      ./run_NuisanceImpacts.sh ${BOSON} ${CHANNEL} ${YEAR}
+      rm *.root *.out
+      echo ""
+    done
+  done
+done
