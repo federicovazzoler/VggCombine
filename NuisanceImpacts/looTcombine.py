3d6535d9766e4b00cf1239e46000877de5937d7f
Federico Vazzoler <federico.vazzoler@ts.infn.it>
2020-03-26 20:43:42 +0100

    add nuisance impact plots but with EXPECTED signal for blind purpouse

---
+++ NuisanceImpacts/looTcombine.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import sys
+import re
+import json
+import ROOT
+import os
+
+from CombineHarvester.CombineTools.combine.CombineToolBase import CombineToolBase
+try:
+    from HiggsAnalysis.CombinedLimit.RooAddPdfFixer import FixAll
+except ImportError:
+    #compatibility for combine version earlier than https://github.com/cms-analysis/HiggsAnalysis-CombinedLimit/tree/2d172ef50fccdfbbc2a499ac8e47bba2d667b95a
+    #can delete in a few months
+    def FixAll(workspace): pass
+
+def all_free_parameters(file, wsp, mc, pois):
+        res = []
+        wsFile = ROOT.TFile.Open(file)
+        w = wsFile.Get(wsp)
+        FixAll(w)
+        config = w.genobj(mc)
+        pdfvars = config.GetPdf().getParameters(config.GetObservables())
+        it = pdfvars.createIterator()
+        var = it.Next()
+        while var:
+            if var.GetName() not in pois and (not var.isConstant()) and var.InheritsFrom("RooRealVar"):
+                res.append(var.GetName())
+            var = it.Next()
+        return res
+
+ws = sys.argv[1]
+rMin = sys.argv[2]
+rMax = sys.argv[3]
+
+POI = 'r'
+
+paramList = all_free_parameters(ws, 'w', 'ModelConfig', POI)
+
+#First fit to r with r fixed to 1 to obtain expected errors on r
+print 'Doing param : ' + POI
+print 'Launching combine as :'
+print 'combine -M MultiDimFit -n _initialFit_Test --algo singles --redefineSignalPOIs {0} --rMin {1} --rMax {2} --robustFit 1 -m 200 -d {3} -t -1 --expectSignal 1'.format(POI, rMin, rMax, ws)
+os.system('combine -M MultiDimFit -n _initialFit_Test --algo singles --redefineSignalPOIs {0} --rMin {1} --rMax {2} --robustFit 1 -m 200 -d {3} -t -1 --expectSignal 1'.format(POI, rMin, rMax, ws))
+
+#Now every nuisance
+for param in paramList:
+  print ''
+  print '-----------------------'
+  print 'Doing param : ' + param
+  print 'Launching combine as :'
+  print 'combine -M MultiDimFit -n _paramFit_Test_{0} --algo impact --redefineSignalPOIs {1} -P {2} --floatOtherPOIs 1 --saveInactivePOI 1 --rMin {3} --rMax {4} --robustFit 1 -m 200 -d {5} -t -1 --expectSignal 1'.format(param, POI, param, rMin, rMax, ws)
+  os.system('combine -M MultiDimFit -n _paramFit_Test_{0} --algo impact --redefineSignalPOIs {1} -P {2} --floatOtherPOIs 1 --saveInactivePOI 1 --rMin {3} --rMax {4} --robustFit 1 -m 200 -d {5} -t -1 --expectSignal 1'.format(param, POI, param, rMin, rMax, ws)) 
--- NuisanceImpacts/run_NuisanceImpacts.sh
+++ NuisanceImpacts/run_NuisanceImpacts.sh
@@ -21,27 +21,42 @@ rm ${FOLDER}/*
 text2workspace.py ../cards/${BOSON}_${CHANNEL}_${YEAR}_datacard.txt  -o ${BOSON}_${CHANNEL}_${YEAR}_workspace.root
 cp ../cards/${BOSON}_${CHANNEL}_${YEAR}_datacard.txt ${FOLDER} 
 
-#initial fit of mu
-combineTool.py -M Impacts -d ${BOSON}_${CHANNEL}_${YEAR}_workspace.root -m 200 --rMin -10 --rMax 10 --robustFit 1 --doInitialFit
-
-#fit separately every nuisance
-combineTool.py -M Impacts -d ${BOSON}_${CHANNEL}_${YEAR}_workspace.root -m 200 --rMin -10 --rMax 10 --robustFit 1 --doFits
+#BLIND
+python looTcombine.py ${BOSON}_${CHANNEL}_${YEAR}_workspace.root 0 4
 
 #collect output and convert to json file
-combineTool.py -M Impacts -d ${BOSON}_${CHANNEL}_${YEAR}_workspace.root -m 200 --rMin -10 --rMax 10 --robustFit 1 --output ${BOSON}_${CHANNEL}_${YEAR}_impacts.json
+combineTool.py -M Impacts -d ${BOSON}_${CHANNEL}_${YEAR}_workspace.root -m 200 --rMin 0 --rMax 4 --robustFit 1 --output ${BOSON}_${CHANNEL}_${YEAR}_impacts.json
 
 #plot the result blind
-plotImpacts.py -i ${BOSON}_${CHANNEL}_${YEAR}_impacts.json -o ${BOSON}_${CHANNEL}_${YEAR}_impacts_blind -t rename.json --blind
+plotImpacts.py -i ${BOSON}_${CHANNEL}_${YEAR}_impacts.json -o ${BOSON}_${CHANNEL}_${YEAR}_impacts_blind -t rename.json
 cp ${BOSON}_${CHANNEL}_${YEAR}_impacts_blind.pdf ${FOLDER}
 
-#plot the result unblind
-plotImpacts.py -i ${BOSON}_${CHANNEL}_${YEAR}_impacts.json -o ${BOSON}_${CHANNEL}_${YEAR}_impacts -t rename.json
-cp ${BOSON}_${CHANNEL}_${YEAR}_impacts.pdf ${FOLDER}
-
-#plot the result unblind
-plotImpacts.py -i ${BOSON}_${CHANNEL}_${YEAR}_impacts.json -o ${BOSON}_${CHANNEL}_${YEAR}_impacts -t rename.json
-cp ${BOSON}_${CHANNEL}_${YEAR}_impacts.pdf ${FOLDER}
-
-#plot the result unblind
+#extract uncertainties blind
 python ExtractNuisance4Paper.py ${BOSON} ${CHANNEL} ${YEAR}
-cp ${BOSON}_${CHANNEL}_${YEAR}_syst_unc.txt ${FOLDER}
+cp ${BOSON}_${CHANNEL}_${YEAR}_syst_unc.txt ${FOLDER}/${BOSON}_${CHANNEL}_${YEAR}_syst_unc_blind.txt
+
+#clean before rerunnig for unblind
+rm combine_logger.out
+rm ${BOSON}_${CHANNEL}_${YEAR}_impacts.json
+rm higgsCombine_initialFit_Test*
+rm higgsCombine_paramFit_Test*
+rm ${BOSON}_${CHANNEL}_${YEAR}_impacts.pdf
+rm ${BOSON}_${CHANNEL}_${YEAR}_syst_unc.txt
+
+#UNBLIND
+#initial fit of mu
+combineTool.py -M Impacts -d ${BOSON}_${CHANNEL}_${YEAR}_workspace.root -m 200 --rMin 0 --rMax 4 --robustFit 1 --doInitialFit
+
+#fit separately every nuisance
+combineTool.py -M Impacts -d ${BOSON}_${CHANNEL}_${YEAR}_workspace.root -m 200 --rMin 0 --rMax 4 --robustFit 1 --doFits
+
+#collect output and convert to json file
+combineTool.py -M Impacts -d ${BOSON}_${CHANNEL}_${YEAR}_workspace.root -m 200 --rMin 0 --rMax 4 --robustFit 1 --output ${BOSON}_${CHANNEL}_${YEAR}_impacts.json
+
+#plot the result unblind
+plotImpacts.py -i ${BOSON}_${CHANNEL}_${YEAR}_impacts.json -o ${BOSON}_${CHANNEL}_${YEAR}_impacts -t rename.json
+cp ${BOSON}_${CHANNEL}_${YEAR}_impacts.pdf ${FOLDER}
+
+#extract uncertainties unblind
+python ExtractNuisance4Paper.py ${BOSON} ${CHANNEL} ${YEAR}
+cp ${BOSON}_${CHANNEL}_${YEAR}_syst_unc.txt ${FOLDER}/${BOSON}_${CHANNEL}_${YEAR}_syst_unc_unblind.txt
