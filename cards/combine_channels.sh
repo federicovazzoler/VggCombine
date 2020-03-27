28ff694dc49d0d4e63ca96f4994e12da0de32acf
Federico Vazzoler <federico.vazzoler@ts.infn.it>
2020-03-27 12:20:59 +0100

    add script for combined datacard for ele and muo channels

---
+++ cards/combine_channels.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+BOSONS="WGG ZGG"
+
+YEARS="2016 2017 2018 Run2"
+
+for BOSON in ${BOSONS}; do
+  for YEAR in ${YEARS}; do
+    echo "Combining ch_ele and ch_muo for ${BOSON} in year ${YEAR}"
+    combineCards.py ch_ele=${BOSON}_ch_ele_${YEAR}_datacard.txt ch_muo=${BOSON}_ch_ele_${YEAR}_datacard.txt > ${BOSON}_ch_lep_${YEAR}_datacard.txt
+  done
+done
