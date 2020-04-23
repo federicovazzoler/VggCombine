e3175f5c8549578c1861bddd420d7a7c9192a939
Federico Vazzoler <federico.vazzoler@ts.infn.it>
2020-04-23 19:01:32 +0200

    Fix my error with previous commit on run_DatacardValidation.sh

---
+++ DatacardValidation/run_DatacardValidation.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+FOLDER="../html/combine_plots/likelihood_scan/DatacardValidation/"
+mkdir -p ${FOLDER}
+
+BOSONS="WGG ZGG"
+CHANNELS="ch_ele ch_muo ch_lep"
+YEARS="2016 2017 2018 Run2"
+
+for BOSON in ${BOSONS}; do
+  for YEAR in ${YEARS}; do
+    for CHANNEL in ${CHANNELS}; do
+      cp ../cards/${BOSON}_${CHANNEL}_${YEAR}_datacard.txt ${FOLDER}
+      echo "--------------------------------------------------"
+      echo "Validating datacard : ${BOSON}_${CHANNEL}_${YEAR}_datacard.txt"
+      echo "--------------------------------------------------"
+      ValidateDatacards.py ../cards/${BOSON}_${CHANNEL}_${YEAR}_datacard.txt -p 3 --jsonFile ${BOSON}_${CHANNEL}_${YEAR}_datacard_validation.json > ${FOLDER}/${BOSON}_${CHANNEL}_${YEAR}_datacard_validation.txt
+      echo ""
+    done
+  done
+done
