062d81b473c15b64fbfadaa844374115aaac1684
Federico Vazzoler <federico.vazzoler@ts.infn.it>
2020-07-10 14:52:38 +0200

    rewrite table according to ARC comment to include statistical errors

--- number_of_events.py
+++ number_of_events.py
@@ -82,7 +82,7 @@ for i in range(len(input_file)):
     bkg_irred_string[i] = ' n.a. +- n.a.'
   for sp in range(16 - len(bkg_irred_string[i])):
     bkg_irred_string[i] += ' '
-  
+
   if i < 2:
     nevt_bkg_egmisid[i][0] = input_file[i].Get('bkg_egmisid').IntegralAndError(0, input_file[i].Get('bkg_egmisid').GetNbinsX()+1, nevt_bkg_egmisid[i][1])
     nevt_sum_mc_tot[i][0] += nevt_bkg_egmisid[i][0]
@@ -100,7 +100,7 @@ for i in range(len(input_file)):
     bkg_egmisid_string[i] = ' n.a. +- n.a.'
     for sp in range(16 - len(bkg_egmisid_string[i])):
       bkg_egmisid_string[i] += ' '
-  
+
   nevt_sum_mc_tot[i][1] = math.sqrt(nevt_sum_mc_tot[i][1])
   nevt_sum_mc_bkg[i][1] = math.sqrt(nevt_sum_mc_bkg[i][1])
 
@@ -110,14 +110,14 @@ for i in range(len(input_file)):
     sum_mc_tot_string[i] = ' n.a. +- n.a.'
   for sp in range(16 - len(sum_mc_tot_string[i])):
     sum_mc_tot_string[i] += ' '
- 
+
   if nevt_sum_mc_bkg[i][0] != ROOT.Double(0):
     sum_mc_bkg_string[i] = ' {0:.1f} +- {1:.1f}'.format(nevt_sum_mc_bkg[i][0], nevt_sum_mc_bkg[i][1])
   else:
     sum_mc_bkg_string[i] = ' n.a. +- n.a.'
   for sp in range(16 - len(sum_mc_bkg_string[i])):
     sum_mc_bkg_string[i] += ' '
-  
+
 # output in table style
 
 output_file.write('|----------------|----------------|----------------|----------------|----------------|\n')
@@ -144,15 +144,15 @@ output_file_tex.write('\\begin{tabular}{|c|c|c|}\n')
 output_file_tex.write('\hline\n')
 output_file_tex.write('Process & $\PWpm\PGg\PGg\\to\Pe\PGne\PGg\PGg$ & $\PWpm\PGg\PGg\\to\PGm\PGnGm\PGg\PGg$\\\\\n')
 output_file_tex.write('\hline\n')
-output_file_tex.write('data & {0:.2f} & {1:.2f} \\\\\n'.format(nevt_data_obs[0][0], nevt_data_obs[1][0]))
+output_file_tex.write('expected signal & ${0:.0f} \\pm {1:.0f}$ & ${2:.0f} \\pm {3:.0f}$\\\\\n'.format(nevt_diboson[0][0], nevt_diboson[0][1], nevt_diboson[1][0], nevt_diboson[1][1]))
+output_file_tex.write('$\mathrm{{j}}\\to\PGg$ misid. & ${0:.0f} \\pm {1:.0f}$ & ${2:.0f} \\pm {3:.0f}$\\\\\n'.format(nevt_bkg_jetpho_misid[0][0], nevt_bkg_jetpho_misid[0][1], nevt_bkg_jetpho_misid[1][0], nevt_bkg_jetpho_misid[1][1]))
+output_file_tex.write('other irreducible + $\PW\PGg$ & ${0:.0f} \\pm {1:.0f}$ & ${2:.0f} \\pm {3:.0f}$\\\\\n'.format(nevt_bkg_irred[0][0], nevt_bkg_irred[0][1], nevt_bkg_irred[1][0], nevt_bkg_irred[1][1]))
+output_file_tex.write('$\PZ\PGg$ & ${0:.0f} \\pm {1:.0f}$ & ${2:.0f} \\pm {3:.0f}$\\\\\n'.format(nevt_bkg_egmisid[0][0], nevt_bkg_egmisid[0][1], nevt_bkg_egmisid[1][0], nevt_bkg_egmisid[1][1]))
 output_file_tex.write('\hline\n')
-output_file_tex.write('expected signal & {0:.2f} & {1:.2f} \\\\\n'.format(nevt_diboson[0][0], nevt_diboson[1][0]))
+output_file_tex.write('predicted backgrounds & ${0:.0f} \\pm {1:.0f}$ & ${2:.0f} \\pm {3:.0f}$\\\\\n'.format(nevt_sum_mc_bkg[0][0], nevt_sum_mc_bkg[0][1], nevt_sum_mc_bkg[1][0], nevt_sum_mc_bkg[1][1]))
+output_file_tex.write('total prediction & ${0:.0f} \\pm {1:.0f}$ & ${2:.0f} \\pm {3:.0f}$\\\\\n'.format(nevt_sum_mc_tot[0][0], nevt_sum_mc_tot[0][1], nevt_sum_mc_tot[1][0], nevt_sum_mc_tot[1][1]))
 output_file_tex.write('\hline\n')
-output_file_tex.write('$\mathrm{{j}}\\to\PGg$ misid. & {0:.2f} & {1:.2f} \\\\\n'.format(nevt_bkg_jetpho_misid[0][0], nevt_bkg_jetpho_misid[1][0]))
-output_file_tex.write('\hline\n')
-output_file_tex.write('other irreducible + $\PW\PGg$ & {0:.2f} & {1:.2f} \\\\\n'.format(nevt_bkg_irred[0][0], nevt_bkg_irred[1][0]))
-output_file_tex.write('\hline\n')
-output_file_tex.write('$\PZ\PGg$ & {0:.2f} & {1:.2f} \\\\\n'.format(nevt_bkg_egmisid[0][0], nevt_bkg_egmisid[1][0]))
+output_file_tex.write('data & ${0:.0f} \\pm {1:.0f}$ & ${2:.0f} \\pm {3:.0f}$\\\\\n'.format(nevt_data_obs[0][0], nevt_data_obs[0][1], nevt_data_obs[1][0], nevt_data_obs[1][1]))
 output_file_tex.write('\hline\n')
 output_file_tex.write('\end{tabular}\n')
 output_file_tex.write('\caption{Summary of the observed and predicted number of events with the full Run II statistics for the $\PW\PGg\PGg$ selection in the electron and muon channels.}\n')
@@ -165,13 +165,14 @@ output_file_tex.write('\\begin{tabular}{|c|c|c|}\n')
 output_file_tex.write('\hline\n')
 output_file_tex.write('Process & $\PZ\PGg\PGg\\to\Pe\Pe\PGg\PGg$ & $\PZ\PGg\PGg\\to\PGm\PGm\PGg\PGg$\\\\\n')
 output_file_tex.write('\hline\n')
-output_file_tex.write('data & {0:.2f} & {1:.2f} \\\\\n'.format(nevt_data_obs[2][0], nevt_data_obs[3][0]))
+output_file_tex.write('expected signal & ${0:.0f} \\pm {1:.0f}$ & ${2:.0f} \\pm {3:.0f}$\\\\\n'.format(nevt_diboson[2][0], nevt_diboson[2][1], nevt_diboson[3][0], nevt_diboson[3][1]))
+output_file_tex.write('$\mathrm{{j}}\\to\PGg$ misid. & ${0:.0f} \\pm {1:.0f}$ & ${2:.0f} \\pm {3:.0f}$\\\\\n'.format(nevt_bkg_jetpho_misid[2][0], nevt_bkg_jetpho_misid[2][1], nevt_bkg_jetpho_misid[3][0], nevt_bkg_jetpho_misid[3][1]))
+output_file_tex.write('other irreducible + $\PW\PGg$ & ${0:.0f} \\pm {1:.0f}$ & ${2:.0f} \\pm {3:.0f}$\\\\\n'.format(nevt_bkg_irred[2][0], nevt_bkg_irred[2][1], nevt_bkg_irred[3][0], nevt_bkg_irred[3][1]))
 output_file_tex.write('\hline\n')
-output_file_tex.write('expected signal & {0:.2f} & {1:.2f} \\\\\n'.format(nevt_diboson[2][0], nevt_diboson[3][0]))
+output_file_tex.write('predicted backgrounds & ${0:.0f} \\pm {1:.0f}$ & ${2:.0f} \\pm {3:.0f}$\\\\\n'.format(nevt_sum_mc_bkg[2][0], nevt_sum_mc_bkg[2][1], nevt_sum_mc_bkg[3][0], nevt_sum_mc_bkg[3][1]))
+output_file_tex.write('total prediction & ${0:.0f} \\pm {1:.0f}$ & ${2:.0f} \\pm {3:.0f}$\\\\\n'.format(nevt_sum_mc_tot[2][0], nevt_sum_mc_tot[2][1], nevt_sum_mc_tot[3][0], nevt_sum_mc_tot[3][1]))
 output_file_tex.write('\hline\n')
-output_file_tex.write('$\mathrm{{j}}\\to\PGg$ misid. & {0:.2f} & {1:.2f} \\\\\n'.format(nevt_bkg_jetpho_misid[2][0], nevt_bkg_jetpho_misid[3][0]))
-output_file_tex.write('\hline\n')
-output_file_tex.write('other irreducible + $\PW\PGg$ & {0:.2f} & {1:.2f} \\\\\n'.format(nevt_bkg_irred[2][0], nevt_bkg_irred[3][0]))
+output_file_tex.write('data & ${0:.0f} \\pm {1:.0f}$ & ${2:.0f} \\pm {3:.0f}$\\\\\n'.format(nevt_data_obs[2][0], nevt_data_obs[2][1], nevt_data_obs[3][0], nevt_data_obs[3][1]))
 output_file_tex.write('\hline\n')
 output_file_tex.write('\end{tabular}\n')
 output_file_tex.write('\caption{Summary of the observed and predicted number of events with the full Run II statistics for the $\PZ\PGg\PGg$ selection in the electron and muon channels.}\n')
