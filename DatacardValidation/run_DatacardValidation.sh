4c5fffc9dd20386524b566c563e1e202ed3982d6
Federico Vazzoler <federico.vazzoler@ts.infn.it>
2020-08-17 14:51:05 +0200

    run only over Run2 datacards

--- DatacardValidation/run_DatacardValidation.sh
+++ DatacardValidation/run_DatacardValidation.sh
@@ -6,7 +6,8 @@ rm ${FOLDER}/*
 
 BOSONS="WGG ZGG"
 CHANNELS="ch_ele ch_muo ch_lep"
-YEARS="2016 2017 2018 Run2"
+#YEARS="2016 2017 2018 Run2"
+YEARS="Run2"
 
 for BOSON in ${BOSONS}; do
   for YEAR in ${YEARS}; do
